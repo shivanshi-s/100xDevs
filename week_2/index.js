c1c19a075598f7026c9de5d7ab8bcfb3d961cc08
shivanshi <shivanshi.saxena2@gmail.com>
2023-06-20 14:42:15 +0530

    example sum app

--- week_2/index.js
+++ week_2/index.js
@@ -2,10 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+function calculateSum(counter) {
+  var sum  = 0;
+  for (var i =0 ; i <= counter; i++){
+    sum = sum + i;
+  }
+  return sum;
+}
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+function handleFirstRequest(req, res) {
+  var counter = req.query.counter;
+  var calculatedSum = calculateSum(counter)
+
+  var answer =  "the sum is " + calculatedSum;
+  res.send(answer);
+}
+
+app.get('/handleSum', handleFirstRequest)
+
+function started(){
+  console.log(`Example app on port ${port}`);
+}
+
+app.listen(port, started);
